1b733f23f5dc2562bc4cdb5edaa1a7b15bc2bb3b
Rakibul Islam <rakibul241221@gmail.com>
2022-11-25 03:55:58 +0600

    basic server setup & connet to database

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 7000
 const app = express()
+require("dotenv").config()
 
 // midleware
 app.use(cors())
 app.use(express.json())
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.farjvzi.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log)
 
 
 app.get('', async (req, res) => {
